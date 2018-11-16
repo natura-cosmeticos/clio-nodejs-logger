6aedfc9700c588c97f4ced30be18480900d242d2
darthaud <daniel@vizir.com.br>
2018-11-16 20:06:30 -0200

    Added Serializer
    A class to serialize log events as needed

---
+++ src/levels.js
@@ -0,0 +1,11 @@
+/**
+ * Default options for logger levels
+ */
+const loggerLevels = Object.freeze({
+  debug: 'debug',
+  error: 'error',
+  log: 'log',
+  warn: 'warn',
+});
+
+module.exports = loggerLevels;
