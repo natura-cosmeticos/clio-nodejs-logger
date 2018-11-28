cf9484020d317586fd2d5121484c449fa8ebe2da
Felipe Dutra <f3liperamos@gmail.com>
2018-11-28 14:35:40 -0200

    Removing conditions on exported function
    Creating an array with matcher functions and using `some` to check
    against conditions

--- src/log-level-filter.js
+++ src/log-level-filter.js
@@ -4,31 +4,25 @@ const {
 
 const LOG_LEVEL = Object.freeze([debug, error, warn, log]);
 
-const debugOutput = ({ logLevel, outputType }) => (
-  logLevel === debug && LOG_LEVEL.includes(outputType)
+/** @private */
+const levelOutputMatchers = [
+  function debugOutput({ logLevel, outputType }) {
+    return logLevel === debug && LOG_LEVEL.includes(outputType);
+  },
+  function errorOutput({ logLevel, outputType }) {
+    return logLevel === error && LOG_LEVEL.slice(1).includes(outputType);
+  },
+  function warnOutput({ logLevel, outputType }) {
+    return logLevel === warn && LOG_LEVEL.slice(2).includes(outputType);
+  },
+  function infoOutput({ logLevel, outputType }) {
+    return logLevel === log && outputType === log;
+  },
+];
+
+/** @private */
+module.exports = ({ logLevel, outputType }) => (
+  levelOutputMatchers.some(matcher => matcher({ logLevel, outputType }))
 );
-
-const errorOutput = ({ logLevel, outputType }) => (
-  logLevel === error && LOG_LEVEL.slice(1).includes(outputType)
-);
-
-const warnOutput = ({ logLevel, outputType }) => (
-  logLevel === warn && LOG_LEVEL.slice(2).includes(outputType)
-);
-
-const infoOutput = ({ logLevel, outputType }) => (
-  logLevel === log && outputType === log
-);
-
-module.exports = function logLevelFilter({ logLevel, outputType }) {
-  if (debugOutput({ logLevel, outputType })
-    || errorOutput({ logLevel, outputType })
-    || warnOutput({ logLevel, outputType })
-    || infoOutput({ logLevel, outputType })) {
-    return true;
-  }
-
-  return false;
-};
 
 module.exports.LOG_LEVEL = LOG_LEVEL;
--- test/unit/log-level-filter.js
+++ test/unit/log-level-filter.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const assert = require('assert').strict;
 const { lorem } = require('faker');
 
 const logLevelFilter = require('../../src/log-level-filter');
@@ -10,23 +10,23 @@ describe('Log Level Filter', () => {
     it('returns true for every known output type', () => {
       const levelDebugCheck = outputType => logLevelFilter({ logLevel: DEBUG, outputType });
 
-      assert.strictEqual(logLevelFilter.LOG_LEVEL.every(levelDebugCheck), true);
+      assert.equal(logLevelFilter.LOG_LEVEL.every(levelDebugCheck), true);
     });
 
     it('returns false if a non-existant output type is requested', () => {
-      assert.strictEqual(logLevelFilter({ logLevel: DEBUG, outputType: lorem.word() }), false);
+      assert.equal(logLevelFilter({ logLevel: DEBUG, outputType: lorem.word() }), false);
     });
   });
 
   context('level: error', () => {
     it('returns false when output type is debug', () => {
-      assert.strictEqual(logLevelFilter({ logLevel: ERROR, outputType: DEBUG }), false);
+      assert.equal(logLevelFilter({ logLevel: ERROR, outputType: DEBUG }), false);
     });
 
     it('returns true for error, warn and info output types', () => {
       const levelErrorCheck = outputType => logLevelFilter({ logLevel: ERROR, outputType });
 
-      assert.strictEqual([ERROR, WARN, LOG].every(levelErrorCheck), true);
+      assert.equal([ERROR, WARN, LOG].every(levelErrorCheck), true);
     });
   });
 
@@ -34,11 +34,11 @@ describe('Log Level Filter', () => {
     const levelWarnCheck = outputType => logLevelFilter({ logLevel: WARN, outputType });
 
     it('retuns false for debug and error output types', () => {
-      assert.strictEqual([DEBUG, ERROR].every(levelWarnCheck), false);
+      assert.equal([DEBUG, ERROR].every(levelWarnCheck), false);
     });
 
     it('returns true for warn and info output types', () => {
-      assert.strictEqual([WARN, LOG].every(levelWarnCheck), true);
+      assert.equal([WARN, LOG].every(levelWarnCheck), true);
     });
   });
 
@@ -46,11 +46,11 @@ describe('Log Level Filter', () => {
     const levelInfoCheck = outputType => logLevelFilter({ logLevel: LOG, outputType });
 
     it('returns false for every type but info', () => {
-      assert.strictEqual([DEBUG, ERROR, WARN].every(levelInfoCheck), false);
+      assert.equal([DEBUG, ERROR, WARN].every(levelInfoCheck), false);
     });
 
     it('returns true when info output type is requested', () => {
-      assert.strictEqual(logLevelFilter({ logLevel: LOG, outputType: LOG }), true);
+      assert.equal(logLevelFilter({ logLevel: LOG, outputType: LOG }), true);
     });
   });
 });
